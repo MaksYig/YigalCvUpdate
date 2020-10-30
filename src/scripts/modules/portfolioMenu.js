3a1f1790aa1d2d982cb4c3fa0dcfc4836fb876fb
Yigal Igor Maksimov <yigal.maksimov@gmail.com>
2020-10-30 21:58:46 +0200

    started offer section

--- src/scripts/modules/portfolioMenu.js
+++ src/scripts/modules/portfolioMenu.js
@@ -5,7 +5,14 @@ function portfolioToogleMenu (){
   try {
     const menu = document.querySelector('.portfolio__menu');
     const portfolioItem = document.querySelectorAll('.portfolio .work-container');
-  
+    const menuBtn = document.querySelectorAll('.portfolio__menu-btn');
+    const jsscript = document.querySelectorAll('[data-cont="jsscript"]');
+    const landing = document.querySelectorAll('[data-cont="landing"]');
+
+    menuBtn[0].firstElementChild.textContent = portfolioItem.length;
+    menuBtn[1].firstElementChild.textContent = landing.length;
+    menuBtn[2].firstElementChild.textContent = jsscript.length;
+    
     menu.addEventListener('click',(e)=>{
       if (e.target && e.target.nodeName ===  "BUTTON"){
         if (e.target.dataset.filter == 'jsscript'){
