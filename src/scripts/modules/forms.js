efbaadfa15d31f915dff3d29a345f3a072fc701d
Yigal Igor Maksimov <yigal.maksimov@gmail.com>
2020-10-31 18:59:27 +0200

    fixed dates and little bugs

--- src/scripts/modules/forms.js
+++ src/scripts/modules/forms.js
@@ -1,6 +1,6 @@
 
- function form (formEl,nameEL,emailEl,messageEl,buttonEl){
-   
+function form(formEl, nameEL, emailEl, messageEl, buttonEl) {
+
   const form = document.querySelector(formEl);
   const username = form.querySelector(nameEL);
   const email = form.querySelector(emailEl);
@@ -20,7 +20,7 @@
         username.parentElement.dataset.status = 'false';
         setErrorFor(username, 'Please fill your Name more than 3 symbols');
         submitButton();
-      } 
+      }
       else {
         setSuccessFor(username);
         username.parentElement.dataset.status = 'true';
@@ -48,7 +48,7 @@
         messageText.parentElement.dataset.status = 'false';
         setErrorFor(messageText, 'Please type your message');
         submitButton();
-      }else if(messageText.value.length < 5) {
+      } else if (messageText.value.length < 5) {
         messageText.parentElement.dataset.status = 'false';
         setErrorFor(messageText, 'Please type message more than 5 symbols');
         submitButton();
@@ -70,7 +70,7 @@
       loading: "Loading.....",
       succes: "Thanks, for for your message. I will contact you soon!",
       failure: "Error",
-      
+
     };
     e.preventDefault();
 
@@ -87,42 +87,62 @@
     `;
     form.append(statusMesage);
 
-/* CONVERT FORMDATA TO JSON OBJECT */
-    const formData = new FormData(form).entries();
-    let jsonObject = {};
-    for (const [key,value] of formData){
-      jsonObject[key] = value;
-    }
-    console.log(jsonObject);
-    const dataBase = 'mail.php';
+    // /* CONVERT FORMDATA TO JSON OBJECT */
+    //     const formData = new FormData(form).entries();
+    //     let jsonObject = {};
+    //     for (const [key,value] of formData){
+    //       jsonObject[key] = value;
+    //     }
+    //     console.log(jsonObject);
+    //     const dataBase = 'mail.php';
 
-   async function postData (url, data){
-      const response = await fetch (url, {
-        method: 'POST',
-        body: JSON.stringify(data),
-        headers:{
-              "Content-type":'aplication/json'
-            }
-      });
-      if (response.ok) {
-        console.log(response.status);
-        
-        // statusMesage.textContent = message.succes;
+    //    async function postData (url, data){
+    //       const response = await fetch (url, {
+    //         method: 'POST',
+    //         body: JSON.stringify(data),
+    //         headers:{
+    //               "Content-type":'aplication/json'
+    //             }
+    //       });
+    //       console.log(data);
+    //       if (response.ok) {
+    //         console.log(response.status);
+
+    //         // statusMesage.textContent = message.succes;
+    //         showThanksModal(message.succes);
+    //         form.reset();
+    //         formReset ();
+
+    //         setTimeout(() => {
+    //           statusMesage.remove();
+    //       }, 3000);
+
+    //       }else{
+    //         console.log(response.status);
+    //         statusMesage.textContent = `${message.failure}, ${response.status} Cant connect to server`;
+    //       }
+
+    //     }
+    //     postData(dataBase,jsonObject);
+
+    const request = new XMLHttpRequest();
+    request.open("POST", "mail.php");
+    const formData = new FormData(form);
+    request.send(formData);
+    request.addEventListener("load", () => {
+      if (request.status === 200) {
+        console.log(request.response);
         showThanksModal(message.succes);
         form.reset();
-        formReset ();
-        
-        setTimeout(() => {
-          statusMesage.remove();
-      }, 3000);
-        
-      }else{
-        console.log(response.status);
-        statusMesage.textContent = `${message.failure}, ${response.status} Cant connect to server`;
+        formReset();
+        statusMesage.remove();
+      } else {
+        console.log(request.response);
+        statusMesage.textContent = `${message.failure}, ${request.response} Cant connect to server`;
       }
-      
-    }
-    postData(dataBase,jsonObject);
+    });
+
+
 
   });
 
@@ -130,8 +150,8 @@
   function submitButton() {
     if (formControl[0].dataset.status === 'true' && formControl[1].dataset.status === 'true' && formControl[2].dataset.status === 'true') {
       button.removeAttribute('disabled');
-    }else{
-      button.setAttribute('disabled','disabled');
+    } else {
+      button.setAttribute('disabled', 'disabled');
     }
   }
 
@@ -160,10 +180,10 @@
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
   }
 
-  function formReset (){
-    formControl.forEach(item =>{
+  function formReset() {
+    formControl.forEach(item => {
       item.dataset.status = 'false';
-      button.setAttribute('disabled','disabled');
+      button.setAttribute('disabled', 'disabled');
       item.querySelector('.check').style.visibility = "hidden";
 
     });
@@ -196,6 +216,6 @@
     modal.style.display = "block";
     document.body.classList.add("active");
   }
- }
+}
 
- export default form;
+export default form;
